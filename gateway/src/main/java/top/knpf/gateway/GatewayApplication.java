924dff7ee46cc4f139dfd005107cdb643764380f
xukainan@fang.com <xukainan@fang.com>
2021-04-14 11:01:06 +0800

    gateway注册到nacos

--- gateway/src/main/java/top/knpf/gateway/GatewayApplication.java
+++ gateway/src/main/java/top/knpf/gateway/GatewayApplication.java
@@ -2,8 +2,13 @@ package top.knpf.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 网关
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
