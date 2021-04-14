057b104789af7b224b99285a60bd998b673a8d5d
xukainan@fang.com <xukainan@fang.com>
2021-04-14 17:29:29 +0800

    配置security失败

--- gateway/src/main/java/top/knpf/gateway/GatewayApplication.java
+++ gateway/src/main/java/top/knpf/gateway/GatewayApplication.java
@@ -3,6 +3,8 @@ package top.knpf.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * 网关
