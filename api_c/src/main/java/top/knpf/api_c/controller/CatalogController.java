de299b974ffe778d9730605f3789719cb2623e6c
xukainan <836127990@qq.com>
2021-05-28 13:55:33 +0800

    C端目录查询接口

--- api_c/src/main/java/top/knpf/api_c/controller/CatalogController.java
+++ api_c/src/main/java/top/knpf/api_c/controller/CatalogController.java
@@ -11,6 +11,7 @@ import top.knpf.service.b.ICatalogService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/c/catalog")
@@ -24,6 +25,40 @@ public class CatalogController {
         BaseResult<List<CatalogDTO>> result = new BaseResult<>();
         List<Catalog> catalogs = catalogService.getNavCatalog();
         return result.renderSuccess(buildDTOs(catalogs));
+    }
+
+    @GetMapping("/list")
+    BaseResult<List<CatalogDTO>> get(){
+        BaseResult<List<CatalogDTO>> result = new BaseResult<>();
+        List<Catalog> catalogs = catalogService.list();
+        List<CatalogDTO> resultDto = buildDto(catalogs);
+        return result.renderSuccess(resultDto);
+    }
+
+    private List<CatalogDTO> buildDto(List<Catalog> catalogs) {
+        List<Catalog> list =
+                catalogs.stream().filter(item -> item.getIsParent() == 1 && item.getParentId() == 0).collect(Collectors.toList());
+        List<CatalogDTO> catalogDTOs = buildTreeData(list, catalogs);
+        return catalogDTOs;
+    }
+
+    private List<CatalogDTO> buildTreeData(List<Catalog> collect, List<Catalog> list) {
+        List<CatalogDTO> dtos = new ArrayList<>();
+        collect.stream().forEach(item -> {
+            CatalogDTO catalogDTO = new CatalogDTO();
+            catalogDTO.setId(item.getId());
+            catalogDTO.setName(item.getName());
+            catalogDTO.setSortOrder(item.getSortOrder());
+            catalogDTO.setIcon(item.getIcon());
+            List<Catalog> nextCollect =
+                    list.stream().filter(next -> next.getParentId() == item.getId()).collect(Collectors.toList());
+            if(nextCollect != null && nextCollect.size() > 0) {
+                List<CatalogDTO> childrenDTOs = buildTreeData(nextCollect, list);
+                catalogDTO.setChildren(childrenDTOs);
+            }
+            dtos.add(catalogDTO);
+        });
+        return dtos;
     }
 
     private List<CatalogDTO> buildDTOs(List<Catalog> catalogs) {
